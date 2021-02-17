9e608432e7b38d2fc64e2945d20615e8bda3b7e0
Amalia Delgado Fernández-Valdés <amaliadfv@gmail.com>
2021-02-17 22:41:20 +0000

    added files

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/* 
+function hero(){
+    $('.banner').slick({
+        autoplay: true,
+        autoplaySpeed: 1000,
+        speed: 2000,
+        dots: true,
+        infinite: true,
+        fade: true,
+        arrows: false
+      });
+}
+*/
+
+$(document).ready(function(){
+    hero();
+});
---
+++ js/roleta.js
@@ -0,0 +1,61 @@
+var tmpAnimation = 0;
+
+var rotate = $('#rotateButton');
+var receber = $('#receberButton');
+var hideText = $('.textoInicio');
+var showText = $('.textoFin');
+
+$("#rotateButton").click(function(){
+    let element = $("#roleta");
+
+    tmpAnimation = tmpAnimation + 1350;
+    $({degrees: tmpAnimation - 1350}).animate({degrees: tmpAnimation}, {
+        duration: 2000,
+        step: function(now) {
+            element.css({
+                transform: 'rotate(' + now + 'deg)'
+            });
+        }
+    });
+
+    $("#rotateButton").click(setTimeout(function(){
+        rotate.hide();
+        receber.show();
+        hideText.hide();
+        showText.show();
+    }, 2500) );
+
+});
+
+$("#iconRodaImg").click(function(){
+    let showMenu = $(".roda");
+    $(showMenu).show();
+    let hideIcon = $(".iconRoda");
+    $(hideIcon).hide();
+});
+
+$("#close").click(function(){
+    let hideMenu = $(".roda");
+    $(hideMenu).hide();
+    let showIcon = $(".iconRoda");
+    $(showIcon).show();
+});
+
+
+
+
+
+    
+
+
+
+/* 
+let triangulo = $('#triangulo');
+    triangulo.animate();
+*/
+
+
+
+
+
+
